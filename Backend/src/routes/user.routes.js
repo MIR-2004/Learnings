7c96a4a9c6f2a23444d2ed8ed9ed82d38f3a65cf
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-03-09 11:13:28 +0530

    tokens

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express"
-import { registerUser } from "../controllers/user.controller.js"
+import { loginUser, logOutUser, registerUser } from "../controllers/user.controller.js"
 import  {upload}  from "../middlewares/multe.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router()
 
@@ -14,6 +15,12 @@ router.route("/register").post(upload.fields(
         maxCount: 1
     }]
 ),registerUser)
+
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,logOutUser)
 
 export default router
 
