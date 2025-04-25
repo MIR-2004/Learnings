2e20cb2e1cf1aacee5b449ff8e799067b4e0f686
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-04-25 21:51:45 +0530

    new

--- 100xDEV/12.4-SQL-Joins-Relatioships/src/index.ts
+++ 100xDEV/12.4-SQL-Joins-Relatioships/src/index.ts
@@ -1,7 +1,9 @@
 import { Client } from "pg";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const client = new Client({
-  connectionString: "postgresql://postgres:mirsaifali@localhost:6543/postgres",
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
 });
 
 async function createUsersTable() {
