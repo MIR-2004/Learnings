8f37bf0564cd82310cb119e6067ac3e8852c2ab1
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-04-29 01:38:45 +0530

    new

---
+++ 100xDEV/14.3-NextJs-Backend/my-app/app/actions/user.ts
@@ -0,0 +1,9 @@
+"use server"
+
+import { PrismaClient } from "@prisma/client/extension";
+
+const client = new PrismaClient();
+
+async function solve(username: String, password: String){
+
+}
--- 100xDEV/14.3-NextJs-Backend/my-app/app/api/user/route.ts
+++ 100xDEV/14.3-NextJs-Backend/my-app/app/api/user/route.ts
@@ -1,15 +1,26 @@
+import { PrismaClient } from "@prisma/client/extension";
 import { NextRequest, NextResponse } from "next/server";
 
+const client = new PrismaClient();
 
-export function GET(req: NextRequest){
+export async function GET(req: NextRequest){
+
+    const user = await client.user.findFirst();
     return NextResponse.json({
-        email: " mir@gamil.com",
-        name:"mir saif ali"
+        email: user.email,
+        name: user.name
     })
 }
 
 export async function POST(req:NextRequest) {
     const body = await req.json();
+
+    client.user.create({
+        data:{
+            name: body.name,
+            password: body.password
+        }
+    })
 
     return NextResponse.json({
         message:"You are logged in"
