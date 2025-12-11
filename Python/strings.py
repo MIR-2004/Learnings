5c3981d72a51f99933a5c6bfae45dde8b09992e8
MIR-2004 <mir.saif.ali2004@gmail.com>
2025-12-11 15:22:00 +0530

    Strings

---
+++ Python/strings.py
@@ -0,0 +1,25 @@
+name = "harry"
+
+print(name[0:3])
+
+print(name[-4: -1])
+
+
+print(name[: 3])
+print(name[2:])
+
+
+#slicing
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+
+print(alphabets[1: 15 : 3])
+
+
+#string functions 
+
+print(name.endswith("ry"))
+print(name.startswith("ha"))
+print(name.capitalize())
+
+
