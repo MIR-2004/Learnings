602502483d08ef2cb6bb5cbc7aedea5031d9ad5a
MIR-2004 <mir.saif.ali2004@gmail.com>
2026-01-06 18:11:20 +0530

    new

---
+++ Python/Fundamentels/dict.py
@@ -0,0 +1,17 @@
+marks = {
+    "Mir" : 7.83,
+    "Jahid" : 8.66,
+    "Arnab" : 6.67,
+    "Nachiketa" : 8.83
+}
+
+
+print(marks["Mir"])
+
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+
+marks.update({"Mir": 7.00})
+
+print(marks.values())
--- Python/Fundamentels/tuple.py
+++ Python/Fundamentels/tuple.py
@@ -1,5 +1,9 @@
 a = (1, 45, 37, 376.398, 45, "Rohan")
 
 no = a.count(45)
-
 print(no)
+
+i = a.index(45)
+print(i)
+
+print("ro" in a)
