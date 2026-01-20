88dd83ab2cb53329ab1080cd8b546fe51c9b49d2
MIR-2004 <mir.saif.ali2004@gmail.com>
2026-01-20 21:11:36 +0530

    new

---
+++ 100xDEV/14.1-NextJs-Intro/my-app/app/(auth)/signin/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function({children} : {
+    children: React.ReactNode
+}){
+    return <div>
+        <div className="border-b p-4">
+            get 20% off
+        </div>
+        {children}
+    </div>
+}
